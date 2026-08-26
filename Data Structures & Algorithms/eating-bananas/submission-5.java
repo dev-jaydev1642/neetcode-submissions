151274ce08ce6dedae207ac061737fd338e34204
dev-jaydev1642 <jaydevchauhan2000@gmail.com>
2026-08-26 23:23:28 +0530

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.java
@@ -0,0 +1,54 @@
+class Solution {
+    // public boolean minEatRate(int[] piles, int mid, int hrs) {
+    //     int minHrs = 0;
+
+    //     for (int i = 0; i < piles.length; i++) {
+    //         minHrs += (piles[i] + mid - 1) / mid;
+    //     }
+
+    //     return minHrs <= hrs;
+    // }
+
+    public boolean minEatRate(int[] piles, int mid, int hrs) {
+        int minHrs = 0;
+
+        for (int i = 0; i < piles.length; i++) {
+            minHrs += (piles[i] + mid - 1) / mid;
+        }
+
+        return minHrs <= hrs;
+    }
+
+    public int minEatingSpeed(int[] piles, int hrs) {
+        // int l = 1, h = Arrays.stream(piles).max().getAsInt();
+        // int res = h;
+
+        // while (l <= h) {
+        //     int mid = l + (h - l) / 2;
+        //     if (minEatRate(piles, mid, hrs)) {
+        //         res = mid;
+        //         h = mid - 1;
+        //     } else {
+        //         l = mid + 1;
+        //     }   
+        // }
+
+        // return res;
+
+        int l = 1, h = Arrays.stream(piles).max().getAsInt();
+
+        int res = h;
+
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (minEatRate(piles, mid, hrs)) {
+                res = mid;
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return res;
+    }
+}
