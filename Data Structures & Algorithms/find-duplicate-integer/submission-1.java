913d4eb2d91b98380e52445621956adc795692a8
dev-jaydev1642 <jaydevchauhan2000@gmail.com>
2026-09-26 10:08:54 +0530

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int tortoise = nums[0];
+        int hare = nums[0];
+
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+
+        int pointer1 = nums[0];
+        int pointer2 = tortoise;
+
+        while (pointer1 != pointer2) {
+            pointer1 = nums[pointer1];
+            pointer2 = nums[pointer2];
+        }
+
+        return pointer1;
+    }
+}
